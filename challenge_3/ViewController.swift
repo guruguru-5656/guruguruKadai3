74c32fc806f79876a0a01f96ed24ef279fd979d6
guruguru-5656 <mori0116takuminn@gmail.com>
2022-04-03 16:50:06 +0900

    課題3完了

--- challenge_3/ViewController.swift
+++ challenge_3/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var firstInputTextField: UITextField!
+    @IBOutlet private weak var secondInputTextField: UITextField!
+    @IBOutlet private weak var firstReverseSwitch: UISwitch!
+    @IBOutlet private weak var secondReverseSwitch: UISwitch!
+    @IBOutlet private weak var firstNumberLabel: UILabel!
+    @IBOutlet private weak var secondNumberLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstReverseSwitch.isOn = false
+        secondReverseSwitch.isOn = false
     }
-
-
+    @IBAction private func calculate(_ sender: Any) {
+        var firstNumber: Int?
+        var secondNumber: Int?
+        if let firstInput = Int(firstInputTextField.text ?? "") {
+            let firstSign = firstReverseSwitch.isOn ? -1 : 1
+            firstNumber = firstInput * firstSign
+            firstNumberLabel.text = String(firstNumber!)
+        } else {
+            firstNumberLabel.text = firstInputTextField.text
+        }
+        if let secondInput = Int(secondInputTextField.text ?? "") {
+            let secondSign = secondReverseSwitch.isOn ? -1 : 1
+            secondNumber = secondInput * secondSign
+            secondNumberLabel.text = String(secondNumber!)
+        } else {
+            secondNumberLabel.text = secondInputTextField.text
+        }
+        guard let firstNumber = firstNumber, let secondNumber = secondNumber
+        else {
+            resultLabel.text = "数字を入力してください"
+            return
+        }
+        resultLabel.text = String(firstNumber + secondNumber)
+    }
 }
-
